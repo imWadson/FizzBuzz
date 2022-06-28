a6e0e40d8add0d0d93e9faf3f6a4c475cbb96f7d
Wadson Nilton <niiltonsk8@gmail.com>
2022-06-27 23:03:26 -0300

    :tada: First commit

---
+++ test/fizz_buzz_test.exs
@@ -0,0 +1,15 @@
+defmodule FizzBuzzTest do
+  use ExUnit.Case
+
+  describe "build/1" do
+    test "when a valid filed provided, return the converted listen" do
+      expected_response = {:ok, [1, 2, :fizz, 4, :buzz, :buzz, :fizzbuz, :buzz]}
+      assert FizzBuzz.build("numbers.txt") == expected_response
+    end
+
+    test "when an invalid filed provided, return the converted an error" do
+      expected_response = {:error, "Error reading the file: enoent"}
+      assert FizzBuzz.build("invalid.txt") == expected_response
+    end
+  end
+end
